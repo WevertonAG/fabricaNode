acc8a1e84921e75f7ea2fd311596dc870f45158f
WevertonAG <weverton552@gmail.com>
2022-11-22 16:43:45 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const port = 8080
 const app = express()
 
 app.get('/produtos', (req, res, next) => {
-  res.send({ nome: 'Notebook', preco: 123.32 })
+  res.send([
+    { nome: 'Notebook', preco: 123.32 },
+    { iphone: 'Iphone', preco: 400.00 }
+  ])
 });
 
 app.listen(port, () => {
-  console.log('listening on port ' + port )
+  console.log('listening on port ' + port)
 });
